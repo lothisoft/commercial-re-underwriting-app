c8d857b3ab600764145c7deb3b0ee2e066f29b8c
lothisoft <lothisoft@yahoo.com>
2019-07-27 22:17:25 -0400

    misc look and feel fixes

--- src/components/organisms/reUnderwritingApp/ReUnderwritingApp.js
+++ src/components/organisms/reUnderwritingApp/ReUnderwritingApp.js
@@ -29,6 +29,7 @@ export class ReUnderwritingApp extends React.Component {
       noiKey: Math.floor(Math.random() * Math.floor(100000000)),
 
       mortgageTerms:[],
+      submitButtonDisabled:true,
     };
 
 
@@ -36,6 +37,7 @@ export class ReUnderwritingApp extends React.Component {
     this.handleRentRowChange = this.handleRentRowChange.bind(this);
     this.handleExpenseChange = this.handleExpenseChange.bind(this);
     this.handleChangeCapRate = this.handleChangeCapRate.bind(this);
+    this.isSubmitButtonDisabled = this.isSubmitButtonDisabled.bind(this);
     this.submit = this.submit.bind(this);
 
   }
@@ -52,32 +54,56 @@ export class ReUnderwritingApp extends React.Component {
     }
 
     const address={};
-    address.street = `${interimAddress.street_number} ${interimAddress.route}`;
+    if (interimAddress.street_number) {
+      address.street = `${interimAddress.street_number} ${interimAddress.route}`;
+    } else {
+      address.street = interimAddress.route;
+    }
     address.city = interimAddress.sublocality_level_1;
     address.state = interimAddress.administrative_area_level_1;
     address.zip = interimAddress.postal_code;
     address.county = interimAddress.administrative_area_level_2;
 
-    this.setState({address});
+    if (address.city === "Manhattan") {
+      address.city = "New York";
+    } else if (address.city === "The Bronx") {
+      address.city = "Bronx";
+    }
+
+    this.setState({address}, this.isSubmitButtonDisabled);
   }
 
   handleRentRowChange(income) {
     let noi = income - this.state.expenses;
-    this.setState({income, noi, noiKey: Math.floor(Math.random() * Math.floor(100000000))});
+    this.setState({income, noi, noiKey: Math.floor(Math.random() * Math.floor(100000000))}, this.isSubmitButtonDisabled);
   }
 
   handleExpenseChange(expenses) {
     let noi = this.state.income - expenses;
-    this.setState({expenses, noi, noiKey: Math.floor(Math.random() * Math.floor(100000000))});
+    this.setState({expenses, noi, noiKey: Math.floor(Math.random() * Math.floor(100000000))}, this.isSubmitButtonDisabled);
   }
 
   handleChangeCapRate(capRate) {
-    this.setState({rate: capRate.value});
+    this.setState({rate: capRate.value}, this.isSubmitButtonDisabled);
   }
 
+  isSubmitButtonDisabled() {
+    const currentState = this.state;
+
+    const isAddressEmpty = Object.entries(currentState.address).length === 0 && currentState.address.constructor === Object;
+    const isIncomeEmpty = currentState.income === 0;
+    const isExpensesEmpty = currentState.expenses === 0;
+    const isNoiEmpty = currentState.noi === 0;
+    const isRateEmpty = currentState.rate === 0;
+
+    const submitButtonDisabled = isAddressEmpty || isIncomeEmpty || isExpensesEmpty || isNoiEmpty || isRateEmpty;
+    this.setState({submitButtonDisabled});
+    }
+
   async submit() {
-    const {address, income, expenses, noi, rate, ..._ } = this.state;
+    const {address, income, expenses, noi, rate} = this.state;
     const postRequestObject = {address, income, expenses, noi, rate};
+    // eslint-disable-next-line no-console
     console.log("POST Request with the following payload:", postRequestObject);
 
     let responseData;
@@ -91,10 +117,11 @@ export class ReUnderwritingApp extends React.Component {
       });
       responseData = await response.json();
     } catch (error) {
-      console.log("Error caught while doing POST request: ", error);
+      // eslint-disable-next-line no-console   // eslint-disable-next-line no-console
+      console.error("Error caught while doing POST request: ", error);
       responseData = JSON.parse("{\"success\":true,\"terms\":[{\"NOI\":277900,\"Value\":86304.34782608695,\"75% LTV Proceeds\":64728.26086956521,\"Treasury\":0.02815,\"Interest Rate\":0.030150000000000003,\"Debt Constant\":0.05068961414695165,\"Annual Debt Service\":222320,\"Years\":30,\"# Payments\":360,\"Payoff\":0,\"Proceeds\":4385908.31162146,\"Agency\":\"Fannie Mae\",\"Type\":\"5 Years Hybrid\"},{\"NOI\":277900,\"Value\":86304.34782608695,\"75% LTV Proceeds\":64728.26086956521,\"Treasury\":0.03105,\"Interest Rate\":0.03305,\"Debt Constant\":0.05258767500507064,\"Annual Debt Service\":222320,\"Years\":30,\"# Payments\":360,\"Payoff\":0,\"Proceeds\":4227606.563297642,\"Agency\":\"Fannie Mae\",\"Type\":\"7 Years Fixed\"},{\"NOI\":277900,\"Value\":86304.34782608695,\"75% LTV Proceeds\":64728.26086956521,\"Treasury\":0.03415,\"Interest Rate\":0.03615,\"Debt Constant\":0.05465863730979345,\"Annual Debt Service\":222320,\"Years\":30,\"# Payments\":360,\"Payoff\":0,\"Proceeds\":4067426.6857393067,\"Agency\":\"Freddie SBL\",\"Type\":\"5 Years Hybrid\"},{\"NOI\":277900,\"Value\":86304.34782608695,\"75% LTV Proceeds\":64728.26086956521,\"Treasury\":0.0364,\"Interest Rate\":0.038400000000000004,\"Debt Constant\":0.056188479327577985,\"Annual Debt Service\":222320,\"Years\":30,\"# Payments\":360,\"Payoff\":0,\"Proceeds\":3956682.9830698525,\"Agency\":\"Freddie SBL\",\"Type\":\"10 Years Fixed\"},{\"NOI\":277900,\"Value\":86304.34782608695,\"75% LTV Proceeds\":64728.26086956521,\"Treasury\":0.0364,\"Interest Rate\":0.038400000000000004,\"Debt Constant\":0.056188479327577985,\"Annual Debt Service\":222320,\"Years\":30,\"# Payments\":360,\"Payoff\":0,\"Proceeds\":3956682.9830698525,\"Agency\":\"Fannie Mae\",\"Type\":\"10 Years Fixed\"},{\"NOI\":277900,\"Value\":86304.34782608695,\"75% LTV Proceeds\":64728.26086956521,\"Treasury\":0.0264,\"Interest Rate\":0.0284,\"Debt Constant\":0.04956288276873429,\"Annual Debt Service\":222319.99999999997,\"Years\":30,\"# Payments\":360,\"Payoff\":0,\"Proceeds\":4485614.790353678,\"Agency\":\"Freddie SBL\",\"Type\":\"5 Years Hybrid\"},{\"NOI\":277900,\"Value\":86304.34782608695,\"75% LTV Proceeds\":64728.26086956521,\"Treasury\":0.0294,\"Interest Rate\":0.0314,\"Debt Constant\":0.05150304303071572,\"Annual Debt Service\":222319.99999999997,\"Years\":30,\"# Payments\":360,\"Payoff\":0,\"Proceeds\":4316638.142476578,\"Agency\":\"Fannie Mae\",\"Type\":\"10 Years Fixed\"},{\"NOI\":277900,\"Value\":86304.34782608695,\"75% LTV Proceeds\":64728.26086956521,\"Treasury\":0.0298,\"Interest Rate\":0.0318,\"Debt Constant\":0.0517648480967802,\"Annual Debt Service\":222320,\"Years\":30,\"# Payments\":360,\"Payoff\":0,\"Proceeds\":4294806.382592832,\"Agency\":\"Fannie Mae\",\"Type\":\"7 Years Fixed\"},{\"NOI\":277900,\"Value\":86304.34782608695,\"75% LTV Proceeds\":64728.26086956521,\"Treasury\":0.0314,\"Interest Rate\":0.0334,\"Debt Constant\":0.05281933394182671,\"Annual Debt Service\":222319.99999999997,\"Years\":30,\"# Payments\":360,\"Payoff\":0,\"Proceeds\":4209064.814123842,\"Agency\":\"Freddie SBL\",\"Type\":\"7 Years Fixed\"}],\"src\":{}}");
     }
-
+    // eslint-disable-next-line no-console
     console.log("Response Data: ", responseData);
     this.setState({mortgageTerms: responseData.terms});
 
@@ -137,10 +164,9 @@ export class ReUnderwritingApp extends React.Component {
                             className="cap-rate"
                             onBlur={this.handleChangeCapRate}/>
 
-           <Button onClick={this.submit} className="submit-button Avenir-LT-Std-95-Black">Submit</Button>
+           <Button onClick={this.submit} className="submit-button Avenir-LT-Std-95-Black" disabled={this.state.submitButtonDisabled}>Submit</Button>
 
            <MortgageTerms terms={this.state.mortgageTerms} visible={this.state.mortgageTerms.length > 0}/>
-
          </div>
        </div>
     );
